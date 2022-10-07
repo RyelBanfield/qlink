f5f24a47bd6801f7eb040adb46f84abd80139cb6
Ryel Banfield <ryelbanfield@gmail.com>
2022-10-07 14:41:09 -0400

    Create initial Footer comp

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer({}) {
+  return (
+    <footer className="footer footer-center bg-base-300 p-4 text-base-content">
+      <div>
+        <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+      </div>
+    </footer>
+  );
+}
