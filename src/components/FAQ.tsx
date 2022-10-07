4f9b3365187503c5f77257d35f82bd54a8553dbb
Ryel Banfield <ryelbanfield@gmail.com>
2022-10-07 14:40:58 -0400

    Create initial FAQ comp

---
+++ src/components/FAQ.tsx
@@ -0,0 +1,52 @@
+export function FAQ({}) {
+  return (
+    <section className="bg-gray-800 text-gray-100">
+      <div className="container mx-auto flex flex-col justify-center p-4 md:p-8">
+        <p className="p-2 text-center text-sm font-medium uppercase tracking-wider">
+          How it works
+        </p>
+        <h2 className="mb-12 text-center text-4xl font-bold leading-none sm:text-5xl">
+          Frequently Asked Questions
+        </h2>
+        <div className="grid gap-10 sm:p-3 md:grid-cols-2 md:gap-8 lg:px-12 xl:px-32">
+          <div>
+            <h3 className="font-semibold">Lorem ipsum dolor sit amet.</h3>
+            <p className="mt-1 text-gray-400">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione,
+              fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum
+              voluptatem consequatur ratione, doloremque debitis? Fuga labore
+              omnis minima, quisquam delectus culpa!
+            </p>
+          </div>
+          <div>
+            <h3 className="font-semibold">Lorem ipsum dolor sit amet.</h3>
+            <p className="mt-1 text-gray-400">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione,
+              fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum
+              voluptatem consequatur ratione, doloremque debitis? Fuga labore
+              omnis minima, quisquam delectus culpa!
+            </p>
+          </div>
+          <div>
+            <h3 className="font-semibold">Lorem ipsum dolor sit amet.</h3>
+            <p className="mt-1 text-gray-400">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione,
+              fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum
+              voluptatem consequatur ratione, doloremque debitis? Fuga labore
+              omnis minima, quisquam delectus culpa!
+            </p>
+          </div>
+          <div>
+            <h3 className="font-semibold">Lorem ipsum dolor sit amet.</h3>
+            <p className="mt-1 text-gray-400">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione,
+              fugit? Aspernatur, ullam enim, odit eaque quia rerum ipsum
+              voluptatem consequatur ratione, doloremque debitis? Fuga labore
+              omnis minima, quisquam delectus culpa!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
