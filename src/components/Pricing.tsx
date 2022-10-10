0f3581853cbd24d3fb2dcbfcfc128866a9035e01
Ryel Banfield <ryelbanfield@gmail.com>
2022-10-09 22:31:10 -0400

    Make Plan comp reuseable and edit details

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -18,6 +18,52 @@ function Feature({ text }: { text: string }) {
   );
 }
 
+function Plan({
+  name,
+  price,
+  description,
+  features,
+  bgColor,
+  buttonColor,
+}: {
+  name: string;
+  price: string;
+  description: string;
+  features: string[];
+  bgColor: string;
+  buttonColor: string;
+}) {
+  const cardClasses = `flex w-full flex-col space-y-6 rounded-md p-6 shadow-lg ${bgColor}`;
+  const buttonClasses = `inline-block w-full rounded px-5 py-3 text-center font-bold tracking-wider ${buttonColor}`;
+
+  return (
+    <div className="flex w-full p-3 md:w-1/2 lg:w-1/3">
+      <div className={cardClasses}>
+        <div>
+          <h4 className="text-2xl font-bold">{name}</h4>
+          <span className="text-6xl font-bold">
+            {price}
+            {price === "Free" ? (
+              ""
+            ) : (
+              <span className="text-sm tracking-wide">/month</span>
+            )}
+          </span>
+        </div>
+        <p className="mt-3 leading-relaxed">{description}</p>
+        <ul className="flex-1 space-y-2">
+          {features.map((feature) => (
+            <Feature key={feature} text={feature} />
+          ))}
+        </ul>
+        <button type="button" className={buttonClasses}>
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+}
+
 export function Pricing({}) {
   return (
     <section className="mb-12 rounded-md bg-neutral-100 px-6 py-12">
@@ -32,66 +78,34 @@ export function Pricing({}) {
         </div>
 
         <div className="flex flex-wrap">
-          <div className="flex w-full p-3 md:w-1/2">
-            <div className="flex w-full flex-col space-y-6 rounded-md bg-gray-900 p-6 shadow-lg ">
-              <div>
-                <h4 className="text-2xl font-bold">Beginner</h4>
-                <span className="text-6xl font-bold">Free</span>
-              </div>
-              <p className="mt-3 leading-relaxed dark:text-gray-400">
-                Just getting started?
-              </p>
-              <ul className="mb-6 flex-1 dark:text-gray-400">
-                <li className="mb-2 flex space-x-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="h-6 w-6 flex-shrink-0 text-blue-700"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>QR Code Generator</span>
-                </li>
-              </ul>
-              <button
-                type="button"
-                className="inline-block rounded bg-blue-700 px-5 py-3 text-center font-semibold tracking-wider"
-              >
-                Get Started
-              </button>
-            </div>
-          </div>
-
-          <div className="flex w-full p-3 md:w-1/2">
-            <div className="flex w-full flex-col space-y-6 rounded bg-blue-700 p-6 shadow">
-              <div className="space-y-2">
-                <h4 className="text-2xl font-bold">Starter</h4>
-                <span className="text-6xl font-bold">
-                  $9
-                  <span className="text-sm tracking-wide">/month</span>
-                </span>
-              </div>
-              <p className="leading-relaxed">
-                For those that need more features.
-              </p>
-              <ul className="flex-1 space-y-2">
-                <Feature text="Everything in Free" />
-                <Feature text="Website Builder" />
-              </ul>
-              <a
-                rel="noopener noreferrer"
-                href="#"
-                className="inline-block w-full rounded bg-neutral-900 px-5 py-3 text-center font-bold tracking-wider"
-              >
-                Get Started
-              </a>
-            </div>
-          </div>
+          <Plan
+            name="Beginner"
+            price="Free"
+            description="Just getting started?"
+            features={["QR Code Generator"]}
+            bgColor="bg-gray-900"
+            buttonColor="bg-blue-700"
+          />
+          <Plan
+            name="Starter"
+            price="$9"
+            description="For those that need more features."
+            features={["QR Code Generator", "Website Builder & Hosting"]}
+            bgColor="bg-blue-700"
+            buttonColor="bg-gray-900"
+          />
+          <Plan
+            name="Pro"
+            price="$19"
+            description="For those that need more features."
+            features={[
+              "QR Code Generator",
+              "Website Builder & Hosting",
+              "Associate Features",
+            ]}
+            bgColor="bg-gray-900"
+            buttonColor="bg-blue-700"
+          />
         </div>
       </div>
     </section>
