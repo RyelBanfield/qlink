099f85cb4e7aa836f8404f9943e9dc8f5b1e3ace
Ryel Banfield <ryelbanfield@gmail.com>
2022-10-18 13:16:51 -0400

    Replace Create code with Link to create page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,33 +1,17 @@
-import { PrismaClient, User } from "@prisma/client";
+import { User } from "@prisma/client";
 import { GetServerSideProps } from "next";
+import Link from "next/link";
 import { getSession } from "next-auth/react";
-import { useState } from "react";
 
-type Props = { user: User };
+import { prisma } from "../server/db/client";
 
-const Dashboard = ({ user }: Props) => {
-  const [credits, setCredits] = useState(user.credits);
-
-  const handleCreateQRCode = async () => {
-    fetch("/api/qr-code", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        userId: user.id,
-        creditsUsed: 1,
-      }),
-    });
-    setCredits(credits - 1);
-    console.log("QR Code created");
-  };
-
+const Dashboard = ({ user }: { user: User }) => {
   return (
     <>
       <section className="mt-3 flex h-min flex-col rounded-md bg-neutral-100 px-3 py-8">
         <h2 className="mb-4 text-center text-3xl font-bold leading-none text-neutral-900 sm:text-3xl">
-          You have <span className="text-blue-700">{credits}</span> QLink Credit
+          You have <span className="text-blue-700">{user.credits}</span> QLink
+          Credit
           {user.credits === 1 ? "" : "s"}
         </h2>
         {user.credits === 0 && (
@@ -45,13 +29,12 @@ const Dashboard = ({ user }: Props) => {
           </form>
         )}
         {user.credits > 0 && (
-          <div
-            className="flex flex-col items-center"
-            onClick={handleCreateQRCode}
-          >
-            <button className="w-44 rounded bg-blue-700 p-2 text-center font-semibold">
-              Create a QLink
-            </button>
+          <div className="flex flex-col items-center">
+            <Link href="/creator">
+              <a className="w-44 rounded bg-blue-700 p-2 text-center font-semibold">
+                Create a QR code
+              </a>
+            </Link>
           </div>
         )}
       </section>
@@ -70,8 +53,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       },
     };
   }
-
-  const prisma = new PrismaClient();
 
   const user = await prisma.user.findUnique({
     where: {
