bdbea829ce64a67019e94a8a625f332652609f99
Ryel Banfield <ryelbanfield@gmail.com>
2022-10-26 13:30:59 -0400

    Fix API warning

--- src/pages/api/qr-code/create.ts
+++ src/pages/api/qr-code/create.ts
@@ -5,49 +5,51 @@ import QRCode from "qrcode";
 import { prisma } from "../../../server/db/client";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
-    const user = req.body.user as User;
+  return new Promise(() => {
+    if (req.method === "POST") {
+      const user = req.body.user as User;
 
-    const { name, link } = req.body;
+      const { name, link } = req.body;
 
-    const options = {
-      margin: 1,
-      width: 300,
-    };
+      const options = {
+        margin: 1,
+        width: 300,
+      };
 
-    QRCode.toDataURL(link, options, async (err, url) => {
-      if (err) res.status(500).json({ error: err });
+      QRCode.toDataURL(link, options, async (err, url) => {
+        if (err) res.status(500).json({ error: err });
 
-      const qrCode = await prisma.qrCode.create({
-        data: {
-          name,
-          url: link,
-          image: url,
-          user: {
-            connect: {
-              id: user.id,
+        const qrCode = await prisma.qrCode.create({
+          data: {
+            name,
+            url: link,
+            image: url,
+            user: {
+              connect: {
+                id: user.id,
+              },
             },
           },
-        },
-      });
+        });
 
-      await prisma.user.update({
-        where: {
-          id: user.id,
-        },
-        data: {
-          credits: {
-            decrement: 1,
+        await prisma.user.update({
+          where: {
+            id: user.id,
           },
-        },
-      });
+          data: {
+            credits: {
+              decrement: 1,
+            },
+          },
+        });
 
-      res.status(200).json({ ...qrCode });
-    });
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
-  }
+        res.status(200).json({ ...qrCode });
+      });
+    } else {
+      res.setHeader("Allow", "POST");
+      res.status(405).end("Method Not Allowed");
+    }
+  });
 };
 
 export default handler;
--- src/pages/api/qr-code/preview.ts
+++ src/pages/api/qr-code/preview.ts
@@ -2,29 +2,31 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import QRCode from "qrcode";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
-    const { name, link } = req.body;
+  return new Promise(() => {
+    if (req.method === "POST") {
+      const { name, link } = req.body;
 
-    const options = {
-      margin: 1,
-      width: 300,
-    };
-
-    QRCode.toDataURL(link, options, async (err, url) => {
-      if (err) res.status(500).json({ error: err });
-
-      const qrCode = {
-        name,
-        url: link,
-        image: url,
+      const options = {
+        margin: 1,
+        width: 300,
       };
 
-      res.status(200).json({ ...qrCode });
-    });
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
-  }
+      QRCode.toDataURL(link, options, (err, url) => {
+        if (err) res.status(500).json({ error: err });
+
+        const qrCode = {
+          name,
+          url: link,
+          image: url,
+        };
+
+        res.status(200).json({ ...qrCode });
+      });
+    } else {
+      res.setHeader("Allow", "POST");
+      res.status(405).end("Method Not Allowed");
+    }
+  });
 };
 
 export default handler;
