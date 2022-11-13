838f05eb668c5c388b5332bc873f071548dd599e
Ryel Banfield <ryelbanfield@gmail.com>
2022-11-13 00:56:08 -0400

    Remove commented code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
-import { GetServerSideProps, NextPage } from "next";
-import { getSession } from "next-auth/react";
+import { NextPage } from "next";
 
 import FAQ from "../components/FAQ";
 import Features from "../components/Features";
@@ -16,22 +15,5 @@ const Home: NextPage = () => {
     </>
   );
 };
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const session = await getSession(context);
-
-//   if (session) {
-//     return {
-//       redirect: {
-//         destination: "/dashboard",
-//         permanent: false,
-//       },
-//     };
-//   }
-
-//   return {
-//     props: { session },
-//   };
-// };
 
 export default Home;
