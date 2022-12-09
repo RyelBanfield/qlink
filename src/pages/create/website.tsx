fa81747645b1ac59ea941c13127214786dc166eb
Ryel Banfield <ryelbanfield@gmail.com>
2022-12-09 12:47:33 -0400

    Create base website create form

--- src/pages/create/website.tsx
+++ src/pages/create/website.tsx
@@ -1,58 +1,147 @@
 import { User, Website } from "@prisma/client";
 import { motion } from "framer-motion";
 import { GetServerSideProps, NextPage } from "next";
+import Image from "next/image";
+// import { useRouter } from "next/router";
 import { getSession } from "next-auth/react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 import { prisma } from "../../server/db/client";
 
-const WebsiteCreator: NextPage<{ user: User }> = ({}) => {
+const WebsiteCreator: NextPage<{ user: User }> = ({ user }) => {
+  // const router = useRouter();
   const { register, handleSubmit } = useForm<Website>();
+
   const [website, setWebsite] = useState<Website | null>(null);
 
   const onSubmit = (data: Website) => {
-    console.log(data);
+    if (data.image && data.image.length > 0) {
+      const reader = new FileReader();
+      const blob = new Blob([data.image[0] as BlobPart], {
+        type: "image/png",
+      });
+      reader.readAsDataURL(blob);
+
+      reader.onload = () => {
+        setWebsite({
+          ...data,
+          image: reader.result as string,
+        });
+      };
+
+      return;
+    }
+
+    data.image = `https://avatars.dicebear.com/api/initials/${data.name
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9 ]/g, "")
+      .replace(/ /g, "-")}.svg`;
+
     setWebsite(data);
+  };
+
+  const onConfirm = (website: Website) => {
+    fetch("/api/website/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user,
+        image: website.image,
+        name: website.name,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
   };
 
   return (
     <>
-      <div className="mb-6 flex min-h-[200px] flex-grow">
-        <div className="grid flex-grow grid-cols-10 rounded bg-white">
-          <div className="col-span-4 p-8">
-            <div className="h-full rounded-3xl border-8 border-neutral-800 bg-black">
-              <div>
-                <h1>{website?.name}</h1>
-              </div>
-            </div>
+      <div className="flex flex-grow flex-col rounded bg-neutral-100 p-6">
+        {/* Display */}
+        <div className="mb-8 w-full sm:w-1/2">
+          <div className="m-auto flex h-[450px] w-1/2 min-w-[220px] flex-col items-center rounded-3xl border-8 border-neutral-800 bg-neutral-100 pt-8">
+            <Image
+              src={
+                website?.image && website.image.length > 0
+                  ? website.image
+                  : website?.name
+                  ? `https://avatars.dicebear.com/api/initials/${website.name}.svg`
+                  : `https://avatars.dicebear.com/api/initials/QLin.svg`
+              }
+              width={50}
+              height={50}
+              alt="Website Logo"
+              className="mb-4 rounded-full"
+            />
+            <h1 className="font-bold text-neutral-900">
+              {website?.name || "Name/Header"}
+            </h1>
           </div>
-          <div className="col-span-6 p-8">
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className="flex h-full flex-col"
-            >
-              <input
-                type="file"
-                accept="image/*"
-                {...register("image")}
-                className="mb-2 rounded border-2 border-gray-300 p-2 text-neutral-900"
-              />
-              <input
-                type="text"
-                placeholder="Enter Name of Website"
-                {...register("name", { required: false })}
-                className="my-2 rounded border-2 border-gray-300 p-2 text-neutral-900"
-              />
+        </div>
+
+        {/* Form */}
+        <div className="flex w-full flex-grow flex-col sm:w-1/2">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mx-12 flex flex-grow flex-col"
+          >
+            <input
+              type="file"
+              accept="image/*"
+              {...register("image", {
+                onChange: () => {
+                  setWebsite(null);
+                },
+              })}
+              className="mb-4 rounded border-2 border-gray-900 p-2 text-neutral-900"
+            />
+            <input
+              type="text"
+              placeholder="Enter Name or Header Website"
+              {...register("name", {
+                required: true,
+                onChange: () => {
+                  setWebsite(null);
+                },
+              })}
+              className="mb-4 rounded border-2 border-gray-900 p-2 text-neutral-900"
+            />
+
+            {!website && (
+              <div className="mt-auto">
+                <motion.button
+                  type="submit"
+                  whileTap={{ scale: 0.95 }}
+                  className="w-full rounded bg-blue-700 p-2"
+                >
+                  Generate Website
+                </motion.button>
+              </div>
+            )}
+          </form>
+
+          {website && (
+            <div className="mx-12 mt-auto flex gap-2">
               <motion.button
                 whileTap={{ scale: 0.95 }}
-                type="submit"
-                className="my-2 mt-auto rounded bg-blue-700 p-2"
+                onClick={() => onConfirm(website)}
+                className="w-1/2 rounded bg-blue-700 p-2"
               >
-                Save
+                Save Website
               </motion.button>
-            </form>
-          </div>
+              <motion.button
+                whileTap={{ scale: 0.95 }}
+                onClick={() => setWebsite(null)}
+                className="w-1/2 rounded bg-red-700 p-2"
+              >
+                Cancel
+              </motion.button>
+            </div>
+          )}
         </div>
       </div>
     </>
